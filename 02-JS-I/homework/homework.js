f4fea4f0fb5d652518312cfa3e766b631fcfea72
sureste <edipean@hotmail.com>
2022-01-02 17:54:36 -0600

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "Hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = 2;
+const nuevoNum = 4;
 
 // Crea una variable booleana:
 const nuevoBool = null;
